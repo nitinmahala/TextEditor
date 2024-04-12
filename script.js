9d078d981c6cfe4afb55b1037ab39b1045be61e7
Nitin Mahala <157006475+nitinmahala@users.noreply.github.com>
2024-04-13 00:07:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -134,7 +134,27 @@ const highlighterRemover = (className) => {
   
 //   // Add event listener to the download button
 //   document.getElementById('downloadPDF').addEventListener('click', downloadPDF);
- 
+click_to_record.addEventListener('click',function(){
+  var speech = true;
+  window.SpeechRecognition = window.webkitSpeechRecognition;
+
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+
+  recognition.addEventListener('result', e => {
+      const transcript = Array.from(e.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+
+      document.getElementById("text-input").innerHTML = transcript;
+      console.log(transcript);
+  });
+  
+  if (speech == true) {
+      recognition.start();
+  }
+})
 
   
 
@@ -142,6 +162,8 @@ const highlighterRemover = (className) => {
   
 
 window.onload = initializer();
-window.onload = downloadPDF();
+
+
+
 
 
